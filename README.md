e2eb8fbc366b98500ef96616dcf5049912c54ba3
johnworker <besscomdark@gmail.com>
2024-06-06 10:01:28 +0800

    調整

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # web_bed_quilt
 棉被宣傳網站
+# 網站目錄
+|-- index.html
+|-- style.css
+|-- style.css.map
+|-- style.scss
+|-- main.js
+|-- /images
+    |-- font_logo.png
+|-- /video
+    |-- Quilt_promo_one.mp4
+    |-- Quilt_promo_two.mp4
--- main.js
+++ main.js
@@ -3,3 +3,20 @@ $(".menu_btn").click(function () {
     $(".menu_wrap").toggleClass("active");
     $(this).toggleClass("active");
   });
+
+
+// 背景影片切換
+$(document).ready(function() {
+  var videoElement = document.getElementById('backgroundVideo');
+  var videoSource = document.getElementById('videoSource');
+  var videos = ['./video/Quilt_promo_one.mp4', './video/Quilt_promo_two.mp4'];
+  var currentVideoIndex = 0;
+
+  videoElement.addEventListener('ended', function() {
+      // 切換到下一個影片
+      currentVideoIndex = (currentVideoIndex + 1) % videos.length;
+      videoSource.src = videos[currentVideoIndex];
+      videoElement.load();
+      videoElement.play();
+  });
+});
